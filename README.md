b1e6be8a6a479233fb650aa5b0685865f56a95b1
kevin-wynn <lukevinskywynn@gmail.com>
2019-02-19 14:46:13 -0600

    initial build

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # vue-bash
-Vue Bash is a tiny component used for showing off bash commands in a beautiful way.
+
+Vue Bash is a simple lightweight component that allows you to show off bash components and commands in a beautiful way.
+
+## Installation
+
+Installation is straightforward and easy. You can install globally in your `main.js` or `import` directly to your components.
+
+1. `npm i --save vue-bash` to install the package
+2. In your `main.js` file `import 'vue-bash'`
+3. Use `<vue-bash/>` anywhere in your app.
+
+## Props
+
+Currently the terminal supports only a few props. But more are to come:
+| Prop | Type | Description |
+|---------|--------|-----------------------------------------------------------------|
+| theme | String | Can accept `light` and `dark` as a theme choice. |
+| width | String | Width of the terminal in either `%` or `px`. Default is `100%` |
+| height | String | Height of the terminal in either `%` or `px`. Default is `100%` |
+| title | String | Title of the terminal. Default is `Terminal` |
+| content | Array | Array of strings per line. |
+
+## Example
+
+````<vue-bash
+          :theme="'dark'"
+          :title="'regen'"
+          :width="'400px'"
+          :height="'250px'"
+          :content="regenContent"
+        />```
+
+```      regenContent: [
+        "regen --mongodb --multipage",
+        "# WELCOME TO REGEN #",
+        "App name: --roboHouse",
+        "User email: --kevin@robo-house.com",
+        "# SETTING UP APP #",
+        "# Installing dependencies...",
+        "# ...",
+        "# ...",
+        "# ...",
+        "# Done"
+      ]```
+````
---
+++ src/components/VueBash/VueBash.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="terminal" :style="{ minWidth: width, minHeight: height }" :class="theme">
+    <div class="header-bar">
+      <div class="actions">
+        <span class="action red"></span>
+        <span class="action yellow"></span>
+        <span class="action green"></span>
+      </div>
+      <div class="title">{{ title }}</div>
+    </div>
+    <div class="content">
+      <ul class="lines" v-for="(line, index) in lines" :key="index">
+        <li class="line" v-html="transformLine(line)"></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VueBash",
+  props: {
+    theme: {
+      type: String,
+      default: "light"
+    },
+    width: {
+      type: String,
+      default: "100%"
+    },
+    height: {
+      type: String,
+      default: "100%"
+    },
+    title: {
+      type: String,
+      default: "Terminal"
+    },
+    content: {
+      type: Array
+    }
+  },
+  data: function() {
+    return {
+      lines: this.content
+    };
+  },
+  methods: {
+    transformLine: function(line) {
+      const commentsRegex = /(\#[\w\'\s\r\n\*]*\#)|(#[\w\s\']*)|(\#[\-\-\s\w\>\/]*#)/gm;
+      const wordsRegex = /"[^"]+"|'[^']+'|\S+/g;
+
+      if (line.match(commentsRegex) != null) {
+        line = line.replace(
+          line.match(commentsRegex)[0],
+          `<span class="comment">${line.match(commentsRegex)[0]}</span>`
+        );
+      }
+
+      let words = line.match(wordsRegex);
+
+      words.forEach((word, i, arr) => {
+        arr[i] =
+          word.indexOf("-") > -1
+            ? `<span class="argument">${word}</span>`
+            : word;
+      });
+
+      return words.join(" ");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.terminal {
+  border: 1px solid #cccccc;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
+  border-radius: 5px;
+}
+
+.header-bar {
+  width: 100%;
+  background: #ececec;
+  padding: 5px;
+  display: flex;
+  justify-content: flex-start;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  border-bottom: 1px solid #cccccc;
+}
+
+.actions {
+  flex-direction: row;
+  margin-left: 5px;
+}
+
+.actions .action {
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  display: inline-flex;
+  margin-right: 5px;
+}
+
+.actions.action.red {
+  background: #e74c3c;
+}
+.actions.action.yellow {
+  background: #f7ca18;
+}
+.actions.action.green {
+  background: #2ecc71;
+}
+
+.title {
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-self: center;
+  margin-left: -50px;
+  font-size: 14px;
+  color: #333;
+}
+
+.content {
+  padding: 15px;
+}
+
+.lines .line {
+  display: block;
+  line-height: 22px;
+  font-family: "Courier New", Courier, monospace;
+}
+
+.line > .argument {
+  color: #2c82c9;
+  font-family: "Courier New", Courier, monospace;
+}
+
+.line > .comment {
+  color: #3fc380;
+  font-family: "Courier New", Courier, monospace;
+}
+
+.terminal.light {
+  background: #fff;
+}
+
+.terminal.light.content.lines.line {
+  color: #666;
+}
+
+.terminal.dark {
+  background: #333;
+}
+
+.terminal.light.content.lines.line {
+  color: #fff;
+}
+</style>
