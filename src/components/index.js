23b2da7e2d2681aa0e8f7a3ac32b0e75826e3679
Kevin Wynn <kevin@robo-house.com>
2019-02-26 16:31:16 -0600

    export vuebash

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,4 @@
 import Vue from "vue";
 import VueBash from "./VueBash";
-const Components = {
-  VueBash
-};
 
-Object.keys(Components).forEach(name => {
-  Vue.component(name, Components[name]);
-});
-
-export default Components;
+export default VueBash;
